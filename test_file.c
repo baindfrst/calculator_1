d36b73e3ee2e692bdffc58e9ccc7278d3df95ab0
baindfrst <n11174@yandex.ru>
2022-04-16 00:31:41 +0300

    initial commit

--- test_file.c
+++ test_file.c
@@ -1,32 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-int charint(int input)
-{
-    int ret = 0;
-    int flag_minus = 0;
-    while (input != 0)
-    {
-        if ('-' == (char)input)
-        {
-            input /= 256;
-            flag_minus = 1;
-        }
-        ret *= 10;
-        ret += (char)(input % 256 ) - (char)'0';
-        input /= 256;
-        printf("%d, %d\n", ret, input);
-    }
-    if (flag_minus)
-    {
-        return -ret;
-    }
-    else
-    {
-        return ret;
-    }
-}
-
 int main()
 {  
     int a;
